c57d20edfa2f80ee899ee30b668e30e4f651eab7
svw83 <shafieevw@shafieevw.com>
2021-06-29 05:50:14 +0800

    init

--- SimUDev.ps1
+++ SimUDev.ps1
@@ -1,45 +1,75 @@
-class Device {
-    [string]$deviceBrand
-    [string]$deviceModel
+enum DeviceType {
+    Undefined = 0
+    Compute = 1
+    Storage = 2
+    Networking = 4
+    Communications = 8
+    Power = 16
+    Rack = 32
+}
 
-    displayDevice(){}
+class Asset {
+    [string]$Brand
+    [string]$Model
+}
 
-    [string]getDeviceBrand(){
-        return $this.deviceBrand
+class Device : Asset {
+    hidden [DeviceType]$devtype = [DeviceType]::Undefined
+    [string]$Status
+
+    [DeviceType] GetDeviceType(){
+        return $this.devtype
     }
 }
 
-class StorageComponents {
-    [int]$storageComponentIndex
-    [string]$storageComponentStatus
+class ComputeServer : Device {
+    hidden [DeviceType]$devtype = [DeviceType]::Compute
+    [string]$ProcessorIdentifier
+    [string]$Hostname
 }
 
-class PowerSupplyUnit : StorageComponents {
-    [string]$storageComponentType = "PSU"
+class Rack : Device {
+    hidden [DeviceType]$devtype = [DeviceType]::Rack
+    hidden [int]$Slots = 8
 
-    [string]getPowerSupplyUnitInformation(){
-        return $this.storageComponentStatus
+    [string]$Datacenter
+    [string]$Location
+    [Device[]]$Devices = [Device[]]::new($this.Slots)
+
+    Rack (){
+        ## Just create the default rack with 8 slots
+    }
+
+    Rack ([int]$s){
+        ## Add argument validation logic here
+        $this.Devices = [Device[]]::new($s)
+    }
+
+    [void] AddDevice([Device]$dev, [int]$slot){
+        ## Add argument validation logic here
+        $this.Devices[$slot] = $dev
+    }
+
+    [void] RemoveDevice([int]$slot){
+        ## Add argument validation logic here
+        $this.Devices[$slot] = $null
     }
 }
 
-class Storage : Device {
-    [PowerSupplyUnit]$powerSupplyUnit
+$FirstRack = [Rack]::new(16)
+$FirstRack.Status = "Operational"
+$FirstRack.Datacenter = "PNW"
+$FirstRack.Location = "F03R02.J10"
 
-    [string]getDevice(){
-        return "I'm a Storage"
-    }
+(0..15).ForEach({
+    $ComputeServer = [ComputeServer]::new()
+    $ComputeServer.Brand = "Fabrikam, Inc."       ## Inherited from Asset
+    $ComputeServer.Model = "Fbk5040"              ## Inherited from Asset
+    $ComputeServer.Status = "Installed"           ## Inherited from Device
+    $ComputeServer.ProcessorIdentifier = "x64"    ## ComputeServer
+    $ComputeServer.Hostname = ("r1s" + $_.ToString("000")) ## ComputeServer
+    $FirstRack.AddDevice($ComputeServer, $_)
+  })
 
-    [void]setPowerSupplyUnit([PowerSupplyUnit]$psu){
-        $this.powerSupplyUnit = $psu
-    }
-}
-
-Clear-Host
-$s = [Storage]::new()
-$s.getDevice()
-$s.deviceBrand = "Dell"
-$s.getDeviceBrand()
-$s.setPowerSupplyUnit([PowerSupplyUnit]::new())
-$s.powerSupplyUnit.storageComponentIndex = 1
-$s.powerSupplyUnit.storageComponentStatus = "Online"
-$s.powerSupplyUnit.getPowerSupplyUnitInformation()
+$FirstRack
+$FirstRack.Devices
