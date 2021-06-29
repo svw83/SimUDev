4c98d6c1528c969bb204cd71e0f8223d710dbfcf
svw83 <shafieevw@shafieevw.com>
2021-06-29 09:53:52 +0800

    s3

--- SimUDev.ps1
+++ SimUDev.ps1
@@ -1,27 +1,113 @@
-class Device {
-    [string]$deviceType
-
-    [string]getDeviceType(){
-        return $this.deviceType
-    }
-}
-class Disk : Device{
-    [int]$diskIndex
-    [string]$diskStatus
+class StorageComponents {
+    [int]$storageComponentsIndex
+    [double]$storageComponentsCapacityGB
     
+    [string]$storageComponentsType
+    [string]$storageComponentsName
+    [string]$storageComponentsStatus
+
+    [string]getStorageComponentsIndex(){
+        return $this.storageComponentsIndex
+    }
+
+    [string]getStorageComponentsCapacityGB(){
+        return $this.storageComponentsCapacityGB
+    }
+
+    [string]getStorageComponentsType(){
+        return $this.storageComponentsType
+    }
+
+    [string]getStorageComponentsName(){
+        return $this.storageComponentsName
+    }
+
+    [string]getStorageComponentsStatus(){
+        return $this.storageComponentsStatus
+    }
+
+    [void]setstorageComponentsIndex([int]$sci){
+        $this.storageComponentsIndex = $sci
+    } 
+
+    [void]setstorageComponentsCapacityGB([double]$scc){
+        $this.storageComponentsCapacityGB = $scc
+    }
+
+    [void]setStorageComponentsType([string]$sct){
+        $this.storageComponentsType = $sct
+    }
+
+    [void]setStorageComponentsName([string]$scn){
+        $this.storageComponentsName = $scn
+    }
+
+    [void]setStorageComponentsStatus([string]$scs){
+        $this.storageComponentsStatus = $scs
+    }
+}
+
+class Disk : StorageComponents {
     Disk(){
-        $this.deviceType = "Disk"
-    }
-}
-class Storage : Device {
-    [Disk[]]$storageDisks
-
-    Storage(){
-        $this.deviceType = "Storage"
+        $this.storageComponentsType = "Disk"
     }
 }
 
-$s = [Storage]::new()
-$s.getDeviceType()
+class PowerSupplyUnit : StorageComponents {
+    PowerSupplyUnit(){
+        $this.storageComponentsType = "PSU"
+    }
+}
 
-#$s | Get-Member
+class Storage {
+    [String]$storageBrand
+    [String]$storageModel
+    [Disk]$storageDisk
+
+    [string]getStorageBrand(){
+        return $this.storageBrand
+    }
+
+    [string]getStorageModel(){
+        return $this.storageModel
+    }
+
+    [void]setStorageBrand([string]$sb){
+        $this.storageBrand = $sb
+    }
+
+    [void]setStorageModel([string]$sm){
+        $this.storageModel = $sm
+    }
+
+    [void]setStorageDisk([Disk]$sd){
+        $this.storageDisk = $sd
+    }
+}
+
+class Equallogic : Storage {
+    Equallogic(){
+        $this.storageBrand = "Dell"
+        $this.storageModel = "Equallogic"
+    }
+}
+
+Clear-Host
+
+$e = [Equallogic]::new()
+
+$e.setStorageDisk([Disk]::new())
+$e.storageDisk.setStorageComponentsIndex(1)
+$e.storageDisk.setStorageComponentsCapacityGB(500.10)
+$e.storageDisk.setStorageComponentsName("Disk1")
+$e.storageDisk.setStorageComponentsStatus("Online")
+$e.storageDisk.setStorageComponentsType("Disk")
+
+#$e.getStorageBrand()
+#$e.getStorageModel()
+#$e.storageDisk.getStorageComponentsName()
+#$e.storageDisk.getStorageComponentsStatus()
+#$e.storageDisk.getStorageComponentsType()
+
+$e
+$e.storageDisk
